d532227cdd5cb5ad6f5bfbad3ea7a180851d0f15
Christie Molloy <cmolloy@redhat.com>
2020-07-21 16:02:10 -0400

    random updates to webpack

--- src/root.component.js
+++ src/root.component.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import { links } from "./root.helper.js";
 import { Link } from "@reach/router";
 
 export default function Root(props) {
   return (
-    <div>
+    <BrowserRouter basename="/">
       <div>
         {links.map((link) => {
           return (
@@ -14,9 +15,31 @@ export default function Root(props) {
           );
         })}
       </div>
-      <div>
-        <a href="https://github.com/react-microfrontends">Github project</a>
-      </div>
-    </div>
+      <Route exact path="/">
+        <Redirect to="/apicuriostudio" />
+      </Route>
+    </BrowserRouter>
   );
 }
+// import React from "react";
+// import { links } from "./root.helper.js";
+// import { Link } from "@reach/router";
+
+// export default function Root(props) {
+//   return (
+//     <div>
+//       <div>
+//         {links.map((link) => {
+//           return (
+//             <Link key={link.href} to={link.href}>
+//               {link.name}
+//             </Link>
+//           );
+//         })}
+//       </div>
+//       <div>
+//         <a href="https://github.com/react-microfrontends">Github project</a>
+//       </div>
+//     </div>
+//   );
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
-const path = require("path");
 
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
