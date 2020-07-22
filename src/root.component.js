f5bd27fc483db641e7283b32cb3dafa37d969f5a
Christie Molloy <cmolloy@redhat.com>
2020-07-22 16:29:59 -0400

    some updates

--- src/root.component.js
+++ src/root.component.js
@@ -1,45 +1,24 @@
 import React from "react";
-import { BrowserRouter, Redirect, Route } from "react-router-dom";
-import { links } from "./root.helper.js";
-import { Link } from "@reach/router";
+// import { BrowserRouter, Redirect, Route } from "react-router-dom";
+// import { links } from "./root.helper.js";
+// import { Link } from "@reach/router";
 
 export default function Root(props) {
   return (
     <BrowserRouter basename="/">
       <div>
-        {links.map((link) => {
+        This is the navigation component.
+        {/* {links.map((link) => {
           return (
             <Link key={link.href} to={link.href}>
               {link.name}
             </Link>
           );
-        })}
+        })} */}
       </div>
-      <Route exact path="/">
+      {/* <Route exact path="/">
         <Redirect to="/apicuriostudio" />
-      </Route>
+      </Route> */}
     </BrowserRouter>
   );
 }
-// import React from "react";
-// import { links } from "./root.helper.js";
-// import { Link } from "@reach/router";
-
-// export default function Root(props) {
-//   return (
-//     <div>
-//       <div>
-//         {links.map((link) => {
-//           return (
-//             <Link key={link.href} to={link.href}>
-//               {link.name}
-//             </Link>
-//           );
-//         })}
-//       </div>
-//       <div>
-//         <a href="https://github.com/react-microfrontends">Github project</a>
-//       </div>
-//     </div>
-//   );
-// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
+const path = require("path");
 
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
