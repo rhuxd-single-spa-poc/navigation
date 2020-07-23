c2d5624fcf7edec71f89ddaa2549c897f2ec2ef9
Christie Molloy <cmolloy@redhat.com>
2020-07-23 13:33:54 -0400

    remove file

--- src/single-spa-config.js
+++
@@ -1,8 +0,0 @@
-import { registerApplication, start } from "single-spa";
-registerApplication(
-  "navigation",
-  () => import("./react-mf-navigation.js").then((module) => module.navigation),
-  () => true
-);
-// registerApplication('home', () => import('./src/home/home.app.js'), () => location.pathname === "" || location.pathname === "/" || location.pathname.startsWith('/home'));
-start();
